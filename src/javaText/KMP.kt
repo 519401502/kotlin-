1706a44b198caed41f432d59430bdd6a4a8cfa98
xvhuichuang <519401502@qq.com>
2017-11-18 00:21:58 +0800

    增加KMP算法

---
+++ src/javaText/KMP.kt
@@ -0,0 +1,58 @@
+package javaText
+
+import java.util.*
+
+/**
+ * Created by 18624915319 on 2017/11/18.
+ */
+class KMP {
+}
+
+fun main(args: Array<String>) {
+    var matching = false
+
+    System.out.println("请输入主字符串")
+    val mainStr: String = Scanner(System.`in`).nextLine()
+    val mainStrNumber = mainStr.length
+    var mainChars = CharArray(mainStrNumber)
+    for(c in 0 .. mainStrNumber - 1){
+        mainChars[c] = mainStr.get(c)
+    }
+
+    System.out.println("请输入子字符串")
+    val sonStr: String = Scanner(System.`in`).nextLine()
+    val sonStrNumber = sonStr.length
+    var sonStrChars = CharArray(sonStrNumber)
+    for(c in 0..sonStrNumber - 1){
+        sonStrChars[c] = sonStr.get(c)
+    }
+
+
+    //算法开始
+    for(i: Int in 0..mainStrNumber - 1 - sonStrNumber){
+        var mainC = mainChars[i]
+        for(j in 0..sonStrNumber - 1){
+            var sonC = sonStrChars[j]
+            if(sonC == mainC){
+                if(j == sonStrNumber - 1){
+                    matching = true;
+                    break
+                }
+                mainC = mainChars[i.plus(1)]
+            } else {
+                mainC = mainChars[i]
+                break
+            }
+        }
+        if(matching){
+            break
+        }
+    }
+    if(matching){
+        System.out.println("匹配成功")
+    } else {
+        System.out.println("匹配失败")
+    }
+
+
+}
